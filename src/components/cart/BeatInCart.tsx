c456aae5c7a98cf7463e65d286f25c6bcbcb9bac
EgemennSahin <egemen.sahin.23@dartmouth.edu>
2023-04-14 14:26:02 -0400

    Fix routing to beat page when removing from cart

--- src/components/cart/BeatInCart.tsx
+++ src/components/cart/BeatInCart.tsx
@@ -10,11 +10,10 @@ export default function BeatInCart({ beatData }: { beatData: BeatData }) {
   const dispatch = useAppDispatch();
 
   return (
-    <Link
-      href={`/beat/${beatData.userId}/${beatData.id}`}
+    <div
       className="relative bg-cover bg-center bg-no-repeat rounded-md group"
       style={{
-        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.4) 50%, rgba(0, 0, 0, 0.8)), url(${beatData.image})`,
+        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8)), url(${beatData.image})`,
       }}
     >
       <div className="p-2">
@@ -29,11 +28,14 @@ export default function BeatInCart({ beatData }: { beatData: BeatData }) {
       </div>
       <button
         onClick={() => dispatch(removeFromCart(beatData))}
-        className="z-10 absolute rounded-r-md top-0 right-0 p-2 bg-red-500 text-white opacity-0 group-hover:opacity-100 group-hover:bg-opacity-100 transition-all duration-200"
+        className="z-20 absolute rounded-r-md top-0 right-0 p-2 bg-red-500 text-white opacity-0 group-hover:opacity-100 group-hover:bg-opacity-100 transition-all duration-200"
       >
         <TrashIcon className="w-5 h-5" />
       </button>
-      <div className="absolute inset-0 rounded-md bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-200"></div>
-    </Link>
+      <Link
+        href={`/beat/${beatData.userId}/${beatData.id}`}
+        className="absolute inset-0 rounded-md bg-black opacity-0 group-hover:opacity-25 transition-opacity duration-200"
+      />
+    </div>
   );
 }
