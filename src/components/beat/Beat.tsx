502fbfb9672c7b0d28026ee87886e0afcb983c4d
EgemennSahin <77211233+EgemennSahin@users.noreply.github.com>
2023-04-22 15:52:29 -0400

    Update mobile ui for beat component

--- src/components/beat/Beat.tsx
+++ src/components/beat/Beat.tsx
@@ -7,7 +7,7 @@ import { getBeatUrl, getUserUrl } from "@/helpers/getRoutings";
 export function Beat({ beatData }: { beatData: BeatData }) {
   return (
     <div className="flex flex-col gap-1 p-2 rounded-md transition-colors hover:bg-base-200 group">
-      <div className="h-32 w-32 lg:h-64 lg:w-64 relative rounded-md overflow-clip hover:cursor-pointer">
+      <div className="h-36 w-36 lg:h-64 lg:w-64 relative rounded-md overflow-clip hover:cursor-pointer">
         <PlayBeatButton beatData={beatData} />
         <Image
           src={beatData.image}
@@ -16,12 +16,12 @@ export function Beat({ beatData }: { beatData: BeatData }) {
           style={{ objectFit: "cover" }}
         />
         <div className="absolute top-0 right-0 w-full h-full bg-gradient-to-tl from-base-300 to-transparent to-50%"></div>
-        <span className="absolute bottom-2 right-3 text-2xl text-primary-content">
+        <span className="absolute bottom-2 right-3 text-md md:text-xl text-primary-content truncate">
           {beatData.price} TL
         </span>
       </div>
       <Link
-        className="text-primary-content text-xl hover:underline"
+        className="text-primary-content w-36 lg:w-64 text-sm md:text-xl hover:underline truncate block"
         href={getBeatUrl(beatData)}
       >
         {beatData.name}
@@ -30,7 +30,7 @@ export function Beat({ beatData }: { beatData: BeatData }) {
       <div className="flex flex-col">
         <Link
           href={getUserUrl(beatData.userId)}
-          className="text-base-content hover:underline"
+          className="text-base-content text-xs md:text-lg hover:underline"
         >
           {beatData.userName}
         </Link>
