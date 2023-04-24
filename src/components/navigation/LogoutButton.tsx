e8e2db30b86ee6c59abadc4c6793715d7592fce8
EgemennSahin <77211233+EgemennSahin@users.noreply.github.com>
2023-04-24 11:47:28 -0400

    Remove unused imports

--- src/components/navigation/LogoutButton.tsx
+++ src/components/navigation/LogoutButton.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { handleLogout } from "@/helpers/auth";
 import { useSupabase } from "@/providers/SupabaseProvider";
 import React from "react";
 export function LogoutButton() {
