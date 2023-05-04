b34a5f26d66efc84f2d305e4cca6272535942e16
EgemennSahin <77211233+EgemennSahin@users.noreply.github.com>
2023-05-04 10:39:51 -0400

    Block transaction if beat already bought

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -1,5 +1,6 @@
 import { SupabaseClient } from "@supabase/auth-helpers-nextjs";
 
+// TODO: Move this to the api once integrated with payment processor
 export async function addTransactionToTable(
   supabase: SupabaseClient,
   beatId: number
@@ -12,6 +13,18 @@ export async function addTransactionToTable(
 
   const user = userData.user;
 
+  // Check if transaction already exists
+  const { data: transactionData } = await supabase
+    .from("transactions")
+    .select("*")
+    .eq("buyer_id", user.id)
+    .eq("beat_id", beatId);
+
+  if (transactionData && transactionData.length > 0) {
+    return transactionData;
+  }
+
+  // Add transaction to table
   const { data, error } = await supabase
     .from("transactions")
     .upsert([{ buyer_id: user.id, beat_id: beatId }]);
